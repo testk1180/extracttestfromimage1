e95d279e1a04d8c2459c14c14976732b549004ea
testk1180 <67689140+testk1180@users.noreply.github.com>
2022-07-19 14:03:41 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using OpenCvSharp;
+using System;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace extractTextfromImage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Hardcoded for testing.
+            string imagePath = @"C:\Users\Kirti.Bodekar\Downloads\Capture.png";
+            Mat img = Cv2.ImRead(imagePath);
+            // Hardcoded for testing. 
+            string tessDataPath = @"D:\ICAP\cap-workbench-1.3.12\cap-workbench-1.3.12\bin\Debug\1tessdata";
+            string result = "";
+            OpenCvSharp.Rect[] textLocations = null;
+            string[] componentTexts = null;
+            float[] confidences = null;
+
+            using (var engine = OpenCvSharp.Text.OCRTesseract.Create(tessDataPath, "eng"))
+            {
+                engine.Run(img, out result, out textLocations, out componentTexts, out confidences, OpenCvSharp.Text.ComponentLevels.TextLine);
+            }
+
+
+
+            Console.WriteLine(result);
+            Console.WriteLine("Hello World!");
+
+        }
+    }
+}
